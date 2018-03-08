880a4534004e1f34de4c70199309a05b89929306
Sekou Dioubate <diousekou@seikkou.com>
2018-03-07 21:37:48 -0500

    Initial commit

---
+++ TestingRepos/TestingRepos/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingRepos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "vs";
+            Console.WriteLine($"The value of VS is {name}");
+        }
+    }
+}
